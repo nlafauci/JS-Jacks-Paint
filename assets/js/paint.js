f6f0f7864dcd555941bb0ae58f25da0110fe75fe
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-05-24 17:20:29 -0400

    updates

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,10 +1,11 @@
 function configureListeners() {
-    var images = document.getElementsByTagName('img') // select img elements  
+    var images = document.getElementsByTagName('img'); // select img elements  
 
 
      for (var i = 0; i < images.length; i++) {        
         document.getElementById(images[i].id.addEventListener('mouseover', addOpacity, false)
-        document.getElementById(images[i].id.addEventListener('mouseout', removeOpacity), false// iterate over images and add mouseover event listeners      
+        document.getElementById(images[i].id.addEventListener('mouseout', removeOpacity), false)
+        // iterate over images and add mouseover event listeners      
     } 
 }
 
@@ -39,57 +40,65 @@ function getProductInfo(paintColor) {
         case 'pn1':
             price = '$14.99'
             colorName = 'Lime Green'
-            updatePrice(colorName, price)                 
+            updatePrice(colorName, price) 
+            break;                 
             // set variables for price and color name and invoke a function to update the price     
-            break;           
+                      
         case 'pn2':
             price = '$11.14'
             colorName = 'Medium Brown'
             updatePrice(colorName, price) 
+            break; 
             // set variables for price and color name and invoke a function to update the price    
-            break;            
+                       
         case 'pn3':
             price = '$22.99'
             colorName = 'Royal Blue'
             updatePrice(colorName, price) 
+            break; 
             // set variables for price and color name and invoke a function to update the price  
-            break;   
+               
         case 'pn4':
             price = '$13.42'
             colorName = 'Solid Red'
-            updatePrice(colorName, price) 
+            updatePrice(colorName, price)
+            break;  
             // set variables for price and color name and invoke a function to update the price  
-            break;   
+               
         case 'pn5':
             price = '$21.98'
             colorName = 'Solid White'
             updatePrice(colorName, price) 
+            break; 
             // set variables for price and color name and invoke a function to update the price       
-            break;   
+               
         case 'pn6':
             price = '$4.99'
             colorName = 'Solid Black'
             updatePrice(colorName, price) 
+            break; 
             // set variables for price and color name and invoke a function to update the price        
-            break;   
+            
         case 'pn7':
             price = '$8.22'
             colorName = 'Solid Cyan'
             updatePrice(colorName, price) 
+            break; 
             // set variables for price and color name and invoke a function to update the price 
-            break;   
+              
         case 'pn8':
             price = '$11.99'
             colorName = 'Solid Purple'
             updatePrice(colorName, price) 
+            break; 
             // set variables for price and color name and invoke a function to update the price   
-            break;   
+              
         case 'pn9':
             price = '$14.99'
             colorName = 'Solid Yellow'
-            updatePrice(colorName, price) 
+            updatePrice(colorName, price)
+            break;  
             // set variables for price and color name and invoke a function to update the price 
-            break;   
           default:              
     }
 
